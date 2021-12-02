53ced2fef4240481fb13138bcd8b8cc53341c287
the-n01se <the_n01se@mail.ru>
2021-12-02 20:05:18 +0700

    prod version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$(function(){
-
+$(document).ready(function () {
+  $(".header__menu").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
+  $(".footer__content").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1000);
+  });
 });
